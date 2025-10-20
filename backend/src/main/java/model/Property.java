d60248acbe23666d2458cd726365156bab69f9fd
LucasCordoba <52933262+lcordoba844@users.noreply.github.com>
2025-10-20 20:01:17 -0300

    Add Property, City, Task and User models.

---
+++ backend/src/main/java/model/Property.java
@@ -0,0 +1,124 @@
+package model;
+
+import java.util.List;
+
+public abstract class Property {
+
+    private int propertyId;
+    private int numberOfBathrooms;
+    private int numberOfBedrooms;
+    private String typeOfProperty;
+    private double price;
+    private boolean parking;
+    private String description;
+    private String address;
+    private City city;
+    private String condition;
+    private String orientation;
+    private List<Task> taskList;
+    private int ownerID;
+
+    public int getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(int propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public int getNumberOfBathrooms() {
+        return numberOfBathrooms;
+    }
+
+    public void setNumberOfBathrooms(int numberOfBathrooms) {
+        this.numberOfBathrooms = numberOfBathrooms;
+    }
+
+    public int getNumberOfBedrooms() {
+        return numberOfBedrooms;
+    }
+
+    public void setNumberOfBedrooms(int numberOfBedrooms) {
+        this.numberOfBedrooms = numberOfBedrooms;
+    }
+
+    public String getTypeOfProperty() {
+        return typeOfProperty;
+    }
+
+    public void setTypeOfProperty(String typeOfProperty) {
+        this.typeOfProperty = typeOfProperty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public int getOwnerID() {
+        return ownerID;
+    }
+
+    public void setOwnerID(int ownerID) {
+        this.ownerID = ownerID;
+    }
+}

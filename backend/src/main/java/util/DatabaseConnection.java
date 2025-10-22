4bb8b96c359218d823573562d1b630836693bf6d
LucasCordoba <52933262+lcordoba844@users.noreply.github.com>
2025-10-21 21:53:41 -0300

    Create config.properties file, remove credentials from DBconnection.

--- backend/src/main/java/util/DatabaseConnection.java
+++ backend/src/main/java/util/DatabaseConnection.java
@@ -1,11 +1,37 @@
 package util;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static Connection conn = null;
+    private static final String url;
+    private static final String dbUser;
+    private static final String dbPassword;
+
+    static {
+        try (InputStream input = DatabaseConnection.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in classpath");
+            }
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            url = prop.getProperty("db.url");
+            dbUser = prop.getProperty("db.user");
+            dbPassword = prop.getProperty("db.password");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
 
     public static Connection connect() throws SQLException {
         try {
@@ -14,15 +40,7 @@ public class DatabaseConnection {
             e.printStackTrace();
             System.exit(1);
         }
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/inmobiliaria?useSSL=false&serverTimezone=UTC", "root", "Guthem121");
 
-        return conn;
+        return DriverManager.getConnection(url, dbUser, dbPassword);
     }
 }
-
-
-
-
-
-
-

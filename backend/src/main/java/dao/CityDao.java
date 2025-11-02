c3fd984d3bef816da5e236759daa8ad1b0a573b1
LucasCordoba <52933262+lcordoba844@users.noreply.github.com>
2025-11-01 21:03:49 -0300

    Add CityDao.

---
+++ backend/src/main/java/dao/CityDao.java
@@ -0,0 +1,83 @@
+package dao;
+
+import model.City;
+import util.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CityDao {
+
+    public static boolean addNewCity(City currentCity) {
+        String sqlQuery = "INSERT INTO cities (description, province_id) VALUES (?, ?)";
+        try (Connection conn = DatabaseConnection.connect();
+            PreparedStatement statement = conn.prepareStatement(sqlQuery);) {
+            statement.setString(1, currentCity.getCityName());
+            statement.setInt(2, currentCity.getProvinceId());
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static City getCityById(int idCity) {
+        String sqlQuery = "SELECT * FROM cities WHERE city_id = ?";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement statement = conn.prepareStatement(sqlQuery)) {
+            statement.setInt(1, idCity);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                City currentCity = new City();
+                currentCity.setCityId(resultSet.getInt("city_id"));
+                currentCity.setCityName(resultSet.getString("description"));
+                currentCity.setProvinceId(resultSet.getInt("province_id"));
+                return currentCity;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<City> getAllCities() {
+        ArrayList<City> listCities = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM cities c";
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement statement = conn.prepareStatement(sqlQuery);
+             ResultSet resultSet = statement.executeQuery();) {
+           while (resultSet.next()) {
+               City currentCity = new City();
+               currentCity.setCityId(resultSet.getInt("city_id"));
+               currentCity.setCityName(resultSet.getString("description"));
+               currentCity.setProvinceId(resultSet.getInt("province_id"));
+               listCities.add(currentCity);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listCities;
+    }
+
+    public static boolean updateCity(City updCity) {
+        String sqlQuery = "UPDATE cities SET cityi_id, city_name = ?, province_id = ? WHERE city_id = ?";
+
+        try (Connection conn = DatabaseConnection.connect();
+             PreparedStatement statement = conn.prepareStatement(sqlQuery)) {
+
+            statement.setInt(1, updCity.getCityId());
+            statement.setString(2, updCity.getCityName());
+            statement.setInt(3, updCity.getProvinceId());
+
+            int rowsUpdated = statement.executeUpdate();
+
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
